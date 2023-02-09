b1c49e67f097d2abf68b6fefe65b3b0ae5535a72
Chris Schnaufer <schnaufer@email.arizona.edu>
2023-02-09 12:09:34 -0700

    Build for CyVerse master catalog

--- entrypoint.stacserver.sh
+++ entrypoint.stacserver.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 # Entrypoint for STAC server docker image
 
-if [ "${STAC_URL}" == "" ]; then
-  STAC_URL="${1}"
-fi
-
-if [ "${STAC_URL}" == "" ]; then
-  echo "STAC Catalog URL not specified on the command line"
-  exit 1
-fi
-
-cd /stac-browser && npm start -- --open "--CATALOG_URL=${STAC_URL}"
+http-server /stac-browser/dist/

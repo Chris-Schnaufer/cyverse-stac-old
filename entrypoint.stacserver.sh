f5b9f642a616ca3f815c014c19ab373ef6e27fc0
Chris Schnaufer <schnaufer@email.arizona.edu>
2023-03-08 11:35:22 -0700

    Revert from certificate changes

--- entrypoint.stacserver.sh
+++ entrypoint.stacserver.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env sh
 # Entrypoint for STAC server docker image
 
-CERT_FILE="${CERT_FILE:-/cert.pem}"
-echo "Looking for default SSL certificate file: ${CERT_FILE}"
-
-HTTPS_OPTIONS=""
-if [ -e "${CERT_FILE}" ]; then
-    HTTPS_OPTIONS="-S -C ${CERT_FILE}"
-    echo "Using HTTPS options: ${HTTPS_OPTIONS}"
-else
-echo "  ... not found"
-fi
-
-http-server /stac-browser/dist/ ${HTTPS_OPTIONS}
+http-server /stac-browser/dist/
